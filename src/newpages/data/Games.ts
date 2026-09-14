69dd28c08f232253561aabf61f3cfb775b3882cc
henrikpeterson <henrikpeterson20@gmail.com>
2026-09-14 09:45:52 +0000

    modification du chemin d'autres images

--- src/newpages/data/Games.ts
+++ src/newpages/data/Games.ts
@@ -5,6 +5,12 @@ import {
   Languages, SpellCheck, BookOpen, MessageCircle
 } from 'lucide-react';
 
+import GamesImg from "@/assets/AfriPuzzle.webp"
+import GamesImg1 from "@/assets/Mots-Meles.webp"
+import GamesImg2 from "@/assets/Math-Puzzle.webp"
+import GamesImg3 from "@/assets/Word-Link.webp"
+
+
 export interface Skill {
   icon: any;
   label: string;
@@ -27,7 +33,7 @@ export const GAMES: Games[] = [
     id: "Puzzle",
     name: "AfriPuzzle",
     description: "Découvre l'Afrique avec des puzzles et développe ta culture générale",
-    image: "src/assets/Games/AfriPuzzle.webp",
+    image: GamesImg,
     color: "#2563EB",
     gradient: "from-blue-600 to-indigo-600",
     path: "/Afri-Puzzle",
@@ -43,7 +49,7 @@ export const GAMES: Games[] = [
     id: "Mots",
     name: "Mots mêlés",
     description: "Améliore ta concentration et ta mémoire tout en solidifiant tes bases en Physique-Chimie",
-    image: "src/assets/Games/Mots-Meles.webp",
+    image: GamesImg1,
     color: "#ed8b3a",
     gradient: "from-purple-600 to-violet-600",
     path: "/jeu-pct",
@@ -59,7 +65,7 @@ export const GAMES: Games[] = [
     id: "Maths",
     name: "MathPuzzle",
     description: "Améliore tes bases en mathématiques, tout en t'amusant",
-    image: "src/assets/Games/Math-Puzzle.webp",
+    image: GamesImg2,
     color: "#059669",
     gradient: "from-green-600 to-emerald-600",
     path: "/math-Puzzle",
@@ -75,7 +81,7 @@ export const GAMES: Games[] = [
     id: "Anglais",
     name: "Word-Link",
     description: "Développe ton vocabulaire, ta grammaire et apprends l'anglais pas à pas",
-    image: "src/assets/Games/Word-Link.webp",
+    image: GamesImg3,
     color: "#1310b9",
     gradient: "from-emerald-600 to-teal-600",
     path: "/Link-Learn",
